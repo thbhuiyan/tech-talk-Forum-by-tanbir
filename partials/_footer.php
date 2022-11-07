610e6f4f8159bb26ea90858e849f6db51e46d5e3
thbhuiyan <th.bhuiyan124578@gmail.com>
2022-11-07 09:26:54 +0600

    nav done

---
+++ partials/_footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <p class="text-center">
+        Copyright tDiscuss Coding Forums 2022 | All rights reserved
+    </p>
+</div>
---
+++ partials/_header.php
@@ -0,0 +1,46 @@
+
+    <?php
+    echo '    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="./images/techtalk.png" height="52px" width="64px" alt="logo of tecktalk">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" tabindex="-1">Contact</a>
+            </li>
+        </ul>
+        <div class="row mx-2">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <button class="btn btn-outline-success ml-2"> Login
+            </button>
+            <button class="btn btn-outline-success mx-2"> Signup
+            </button>
+        </div>
+
+    </div>
+</nav>';
+    ?>
